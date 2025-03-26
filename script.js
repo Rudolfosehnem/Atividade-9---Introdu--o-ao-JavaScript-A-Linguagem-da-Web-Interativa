05c705cb1a99f4dc3711d4f49ea2fad9842df618
Rudolfo <rudolfo_silva@estudante.sesisenai.org.br>
2025-03-26 08:27:15 -0300

    update-3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function verificarPar(numero){
     if(numero % 2 === 0){
         console.log("par");
@@ -17,7 +16,7 @@ verificarPar(7)
 function calcular(n1, n2){
     const soma = n1 + n2;
     const subtracao = n1 - n2;
-    const divisao = n2 !== 0 ? n1/n2: "nao dive";
+    const divisao = n2 !== 0 ? n1/n2: "nao é divisivel";
     const multiplicacao = n1 * n2;
 
     console.log(soma);
@@ -36,7 +35,11 @@ let delay =0;
 while (numero >=1){
     console.log(numero);
     numero--;
-}  
+}
+
+
+
+
 
 function inverterTexto(texto) {
     return texto.split('').reverse().join('');
@@ -53,7 +56,7 @@ function contarCaracteres(texto) {
     return texto.length;
 }
 
-console.log(contarCaracteres("Eai_bom"));
+console.log(contarCaracteres("oii"));
 
 
 
@@ -68,3 +71,55 @@ const carro = {
 console.log(carro.marca);
 console.log(carro.ano);
 console.log(carro.modelo);
+
+
+
+
+
+
+function mensagemPersonalizada(nome, mensagem = "oii") {
+    console.log(`${mensagem} ${nome}`);
+}
+
+
+mensagemPersonalizada("Éwerton"); 
+
+mensagemPersonalizada("Éwerton", "Boa noite!"); 
+
+
+
+
+
+
+function media(n1, n2, n3) {
+    return (n1 + n2 + n3) / 3;
+}
+
+console.log(media(7, 8, 9));
+
+
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 3 === 0) {
+        console.log(`${i}: Múltiplo de 3`);
+    } else {
+        console.log(`${i}: Não múltiplo de 3`);
+    }
+}
+
+
+
+
+
+function verificarPalindromo(palavra) {
+    let invertida = palavra.split('').reverse().join('');
+    return palavra === invertida;
+}
+
+
+console.log(verificarPalindromo("hello")); 
+
+console.log(verificarPalindromo("arara")); 
